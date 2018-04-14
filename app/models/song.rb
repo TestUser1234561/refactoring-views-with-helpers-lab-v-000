d60b0314ab5bca04eea3b4009b1c9ecd4da89f72
Morgan Fudge <mfudge92@gmail.com>
2018-04-14 03:21:08 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name=(name)
+  def song_name=(name)
     self.song = Song.find_or_create_by(name:name)
   end
 
-  def artist_name
+  def song_name
     self.song.name
   end
 end
