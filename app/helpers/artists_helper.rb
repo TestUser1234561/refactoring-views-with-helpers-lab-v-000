86e7689f952702ca7cf1942b471c700aa83ae83e
Morgan Fudge <mfudge92@gmail.com>
2018-04-14 03:18:26 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,9 +2,9 @@ module ArtistsHelper
   def display_artist(artist)
     @a = Artist.find(artist)
     if Artist.song
-      link_to Artist.song.name, Artist.song
+      link_to @a.song.name, @a.song
     else
-      link_to 'Add Artist', edit_song_path(song)
+      link_to 'Add Artist', edit_song_path(@a.song)
     end
   end
 end
